8db0dfa984ec488acafbc053b62b0914742e8d97
Reshan Gomis <reshangomis@gmail.com>
2024-03-13 11:14:54 +0530

    added JWT secret key to .env file

--- backend/controllers/petOwnerController.js
+++ backend/controllers/petOwnerController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, "dhfsdfioshdfoishdoifhsd", { expiresIn: '3d' })
+    return jwt.sign({_id}, process.env.SECRET , { expiresIn: '3d' })
 }
 
 const login = async (req, res) => {
