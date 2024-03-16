c702900148338bb73cc28d3816d4a1ca2214ee82
Hirusha Jayarathne <116256101+Hirusha-Chamod@users.noreply.github.com>
2024-03-16 16:29:21 +0530

    backend done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express =  require('express');
 const mongoose = require("mongoose")
 const cors = require("cors");
+const adoptionFormRoutes = require('./routes/routes.js')
 
 const app = express()
 
@@ -12,9 +13,18 @@ const corsOptions ={
 
 app.use(cors(corsOptions))
 
+//middleware
 app.use(express.json())
 
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+  })
+  
+//Routes
+app.use('/api/routes', adoptionFormRoutes)
 
+//Connect to db
 mongoose.connect("mongodb+srv://reshangomis:dSq14MkjCGA5mxz3@itp-project.4h3vlxi.mongodb.net/?retryWrites=true&w=majority&appName=ITP-Project")
     .then(() => {
         const PORT = app.listen(4000, () => {
