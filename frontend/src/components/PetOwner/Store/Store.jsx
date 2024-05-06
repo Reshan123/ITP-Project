d693bf8370807b237e142c6d190e4eea38145783
FR0n1x <105339444+FR0n1x@users.noreply.github.com>
2024-05-06 17:11:12 +0530

    updated the store

--- frontend/src/components/PetOwner/Store/Store.jsx
+++ frontend/src/components/PetOwner/Store/Store.jsx
@@ -25,13 +25,17 @@ const Store = ({ navBarProps }) => {
         fetchInventoryItems();
     }, [dispatch]);
 
-    const reduceStock = async () => {
+    const handleClick = async () => {
+        // Raveesha's Code
 
     };
 
 
     function renderInventoryItem(inventoryitem) {
         if (inventoryitem.currentStock >= 5) {
+            // Define a range of quantities (e.g., from 1 to 10)
+            const quantityOptions = Array.from({ length: Math.min(inventoryitem.currentStock, 3) }, (_, index) => index + 1);
+
             return (
                 <div className="card">
                     <div className="image">
@@ -39,13 +43,24 @@ const Store = ({ navBarProps }) => {
                     </div>
                     <div className="desc">In Stock: {inventoryitem.currentStock}</div>
                     <div className="title">{inventoryitem.itemName}</div>
-                    <div className="box">
+                    <div className="description">{inventoryitem.itemDescription}</div>
+                    <br />
                         <div className="price">{inventoryitem.itemPrice} LKR</div>
-                        <button className="btn">View More</button>
+                    <div className="box">
+                        <div className="quantity">
+                            <label>Select the Quantity</label>
+                            <input
+                                type='number'
+                                onChange={(e) => (e.target.value)}
+                                value
+                            />
+                        </div>
+                        <button onClick={handleClick} className="btn">Buy</button>
                     </div>
                 </div>
             );
-        } else {
+        }
+        else {
             return (
                 <div className="card">
                     <div className="image">
@@ -53,6 +68,7 @@ const Store = ({ navBarProps }) => {
                     </div>
                     <div className="desc" style={{ color: 'red' }}>Out of Stock</div>
                     <div className="title">{inventoryitem.itemName}</div>
+                    <div className="description">{inventoryitem.itemDescription}</div>
                     <div className="box">
                         <div className="price">{inventoryitem.itemPrice} LKR</div>
                     </div>
