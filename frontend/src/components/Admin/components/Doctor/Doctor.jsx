ab19ffc4ffa7d8682788508e2b06730def4b1824
FR0n1x <105339444+FR0n1x@users.noreply.github.com>
2024-05-13 21:14:47 +0530

    added print function to user and doctor

--- frontend/src/components/Admin/components/Doctor/Doctor.jsx
+++ frontend/src/components/Admin/components/Doctor/Doctor.jsx
@@ -2,12 +2,14 @@ import { useAllDocContext } from "../../../../hooks/useAllDoctorContext";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import './styles.css'
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
 
 const Doctor = () => {
 
     const [searchQuery, setSearchQuery] = useState("")
     const [currentlyDisplayedItem, setCurrentlyDisplayedItems] = useState([])
-    const {doctors, dispatch:allDocDispatch} = useAllDocContext()
+    const { doctors, dispatch: allDocDispatch } = useAllDocContext()
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -15,11 +17,11 @@ const Doctor = () => {
     }, [doctors])
 
     useEffect(() => {
-        if (doctors){
-            const filteredList = doctors.filter(doc => { 
-                return ((doc.name.toLowerCase().includes(searchQuery.toLowerCase()))  ||
-                        (doc.email.toLowerCase().startsWith(searchQuery.toLowerCase())) ||
-                        ((doc.contactNo.toString()).includes(searchQuery)))
+        if (doctors) {
+            const filteredList = doctors.filter(doc => {
+                return ((doc.name.toLowerCase().includes(searchQuery.toLowerCase())) ||
+                    (doc.email.toLowerCase().startsWith(searchQuery.toLowerCase())) ||
+                    ((doc.contactNo.toString()).includes(searchQuery)))
             })
             setCurrentlyDisplayedItems(filteredList)
         }
@@ -27,29 +29,86 @@ const Doctor = () => {
 
     const deleteDoc = async (docID) => {
         const confirmed = confirm("Are You Sure?")
-        if (confirmed){
-            try{
+        if (confirmed) {
+            try {
 
                 const config = {
                     method: 'DELETE',
                 }
                 const response = await fetch(`http://localhost:4000/api/doctor/deleteDoctorFromID/${docID}`, config);
                 const json = await response.json()
-    
-                if(!response.ok){
+
+                if (!response.ok) {
                     throw Error(json.message)
                 }
-    
-                allDocDispatch({type: "DELETE DOCTOR", payload:docID})
+
+                allDocDispatch({ type: "DELETE DOCTOR", payload: docID })
                 navigate('/admin/home/doctor')
-    
-            } catch (error){
+
+            } catch (error) {
                 console.log(error.message)
             }
         }
     }
 
-    return ( 
+    //Download Report Content
+    const generatePDF = () => {
+        const doc = new jsPDF();
+
+        // Get the current date
+        const currentDate = new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
+        doc.text(`Appointment Booking Report - ${currentDate}`, 14, 12);
+
+        // Define the columns and rows for the table
+        const columns = [
+
+            { header: 'Name', dataKey: 'name' },
+            { header: 'Email', dataKey: 'email' },
+            { header: 'Contact No', dataKey: 'contactNo' },
+            { header: 'Availability', dataKey: 'availability' },
+        ];
+
+        const rows = currentlyDisplayedItem.map((doc) => ({
+
+            name: doc.name,
+            email: doc.email,
+            contactNo: doc.contactNo,
+            availability: doc.availability,
+        }));
+
+        // Add the table to the PDF
+        doc.autoTable({
+            columns,
+            body: rows,
+            startY: 20,
+            styles: {
+                // Styles applied to the table
+                cellPadding: 2,
+                fontSize: 10,
+                valign: 'middle',
+                halign: 'center',
+                cellWidth: 'auto', // Auto column width
+            },
+            headStyles: {
+                fillColor: [100, 100, 100], // Header background color
+                textColor: [255, 255, 255], // Header text color
+                fontStyle: 'bold', // Bold font for header
+            },
+            bodyStyles: {
+                textColor: [50, 50, 50], // Body text color
+            },
+            alternateRowStyles: {
+                fillColor: [245, 245, 245], // Alternate row background color
+            },
+        });
+
+        // Save the PDF with a unique name
+        const filename = 'doctoryReport.pdf';
+        doc.save(filename);
+    };
+
+    return (
         <>
             <div className="allDoctorsPage">
                 <div className="allDoctorsHeader">
@@ -57,7 +116,7 @@ const Doctor = () => {
                     <div>
                         <input type="text" name="" id="" placeholder="Search Text" value={searchQuery} onChange={e => setSearchQuery(e.target.value)} />
                         <button onClick={() => navigate('/admin/home/createdoctor')}>Add Doctors</button>
-                        <button>Print</button>
+                        <button onClick={generatePDF}>Print</button>
                     </div>
                 </div>
                 <hr />
@@ -72,26 +131,26 @@ const Doctor = () => {
                             </tr>
                         </thead>
                         <tbody>
-                        {currentlyDisplayedItem && currentlyDisplayedItem.map(doc => (
-                            <tr key={doc._id}>
-                                <td>{doc.name}</td>
-                                <td>{doc.email}</td>
-                                <td>{doc.contactNo}</td>
-                                <td>{doc.availability ? (<div style={{color:"green"}}>Available</div>) : (<div style={{color:"red"}}>Unavailable</div>)}</td>
-                                <td>
-                                    <center>
-                                        <button className="table-view-btn" onClick={() => navigate(`/admin/home/updatedoctor/${doc._id}`)}>Update</button>
-                                        <button className="table-view-btn" onClick={() => deleteDoc(doc._id)}>Delete</button>
-                                    </center>
-                                </td>
-                            </tr>
-                        ))}  
+                            {currentlyDisplayedItem && currentlyDisplayedItem.map(doc => (
+                                <tr key={doc._id}>
+                                    <td>{doc.name}</td>
+                                    <td>{doc.email}</td>
+                                    <td>{doc.contactNo}</td>
+                                    <td>{doc.availability ? (<div style={{ color: "green" }}>Available</div>) : (<div style={{ color: "red" }}>Unavailable</div>)}</td>
+                                    <td>
+                                        <center>
+                                            <button className="table-view-btn" onClick={() => navigate(`/admin/home/updatedoctor/${doc._id}`)}>Update</button>
+                                            <button className="table-view-btn" onClick={() => deleteDoc(doc._id)}>Delete</button>
+                                        </center>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
             </div>
         </>
-     );
+    );
 }
- 
+
 export default Doctor;
